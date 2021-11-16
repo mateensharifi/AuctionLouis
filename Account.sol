c0743cbff9638341e1ee1b776da37b6ee39802ec
mateensharifi <mateensharifi11@gmail.com>
2021-11-16 14:36:04 -0800

    Removed getUsername in Account

--- Account.sol
+++ Account.sol
@@ -3,14 +3,9 @@ pragma solidity  >=0.8.9;
 contract Acount {
     string private username;
     string private userID;
-      address private creator;
+    address private creator;
     
     constructor () {
         creator = msg.sender;
     }
-    
-    // function getUsername() public return (string) {
-    //     return username;
-    // }
-    
 }
